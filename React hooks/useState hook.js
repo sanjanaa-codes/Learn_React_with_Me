6fd3f82f7466d7f1f2b48b0797e12ba43affa998
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-10-12 16:35:45 +0530

    Update useState hook.js

--- React hooks/useState hook.js
+++ React hooks/useState hook.js
@@ -228,6 +228,47 @@ ________________________________________________________________________________
 | Form resets            | Inputs become empty again   |
 
 ---
+_______________________________________________________________________________________________
+
+Question 1
+Is this a valid useState hook invocation and destructuring?
+
+const [car, setCar] = useState({ color: 'blue', mileage: 0})
+
+-> Yes
+-> No
+-> It would be valid, if it was spread over multiple lines.
+
+
+Correct. This code shows a valid call to the useState() hook. It's also correctly destructured to car and setCar.
+Ans-> Yes
+-----------------------------------------------------------------------------------------------------------------
+Question 2
+True or False: You can clone a JS object using the . operator (the dot operator).
+
+
+-> True
+-> False
+
+Correct. The dot operator is not used to clone an object in JS.
+Ans-> False
+------------------------------------------------------------------------------------------------------------------
+Question 3
+Consider the following code:
+
+const [person, setPerson] = useState({ name: 'John', age: 21})
+
+Imagine you're using a setPerson() state-updating function to update the value of the state variable named person. 
+You only want to update the value of age, from 21 to 22. Choose the correct code snippet to do that.
+
+-> setPerson(prev => ({ ...prev, age: 22 }));
+-> setPerson(() => ({ age: 22 }));
+-> setPerson(person.age = 22);
+
+
+Correct
+Yes, this snippet is correct, because it clones the previous state object, and updates only the cloned object's age value.
+Ans-> setPerson(prev => ({ ...prev, age: 22 }));
 
 
   
